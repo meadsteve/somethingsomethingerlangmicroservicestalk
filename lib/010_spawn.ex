f66c31b5fd1fc99dac37327dd7ade2607d19e276
steve.brazier <steve.b@lendinvest.com>
2017-02-28 11:41:38 +0000

    add extra messages in spawn example

--- lib/010_spawn.ex
+++ lib/010_spawn.ex
@@ -1,6 +1,6 @@
 defmodule Spawn do
   def run() do
-    spawn(fn -> IO.write "hello from process one\n" end)
-    spawn(fn -> IO.write "hello from process two\n" end)
+    spawn(fn -> IO.write "hello from process one (id: #{inspect self()})\n" end)
+    spawn(fn -> IO.write "hello from process two (id: #{inspect self()})\n" end)
   end
 end
