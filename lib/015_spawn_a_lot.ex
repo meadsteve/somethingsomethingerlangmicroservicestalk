2cce2e8706753808fc1940450b97611a71f19728
steve <meadsteve@gmail.com>
2019-03-31 14:26:55 +0200

    add task async example

--- lib/015_spawn_a_lot.ex
+++ lib/015_spawn_a_lot.ex
@@ -2,8 +2,26 @@ defmodule SpawnALot do
   def run() do
     1..10_000 |> Enum.map(fn n -> spawn(fn ->
       :timer.sleep(:rand.uniform(100))
-      IO.write "hello from process #{n}\n" end)
-    end)
+      IO.write "hello from process #{n}\n"
+      n
+    end) end)
     :ok
+  end
+end
+
+
+
+
+
+
+defmodule HideTheDetails do
+  def run() do
+    stream = 1..100 |> Task.async_stream(fn n ->
+      :timer.sleep(:rand.uniform(100))
+      IO.write "hello from process #{n}\n"
+      n
+    end, max_concurrency: 10)
+
+    Enum.reduce(stream, 0, fn {:ok, n}, total -> total + n end)
   end
 end
