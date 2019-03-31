e892075b44915fd45e0a30c18757af4ad7db98ba
steve <meadsteve@gmail.com>
2019-03-31 14:00:05 +0200

    Bump to 1.7

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SomethingSomething.Mixfile do
   def project do
     [app: :something_something,
      version: "0.1.0",
-     elixir: "~> 1.3",
+     elixir: "~> 1.7",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
